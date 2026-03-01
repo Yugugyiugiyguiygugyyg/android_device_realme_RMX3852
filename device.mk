3d9dcc994efa88eb09e3f82294211fa402cf591d
MisterZtr <andey2004586@gmail.com>
2026-03-01 17:19:40 +0300

    RMX3852: Remove non-working vibration effects

--- device.mk
+++ device.mk
@@ -50,8 +50,6 @@ $(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
 PRODUCT_PACKAGES += \
     android.hardware.vibrator.service.oplus-richtap
 
-$(call soong_config_set,OPLUS_LINEAGE_VIBRATOR_HAL,INCLUDE_DIR,$(LOCAL_PATH)/vibrator/include)
-
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
